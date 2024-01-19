cb99036926d35c6133c0e3179fdde5f9ab4035da
Owen Marchywka <67765181+dcfalcons2025@users.noreply.github.com>
2024-01-18 23:56:10 -0500

    Created scheduling for the round robin

--- league.py
+++ league.py
@@ -1,12 +1,26 @@
-from team import Team
-
 class League:
-    
-    # Creates an array for the teams in the league
     def __init__(self):
         self.teams = []
 
-    #Adds a team to the league
     def add_team(self, team):
         if team not in self.teams:
             self.teams.append(team)
+
+    def schedule_round_robin(self):
+        num_teams = len(self.teams)
+        if num_teams % 2 != 0:
+            self.teams.append(None)  # Add a placeholder if the number of teams is odd
+
+        for _ in range(num_teams - 1):
+            print("Round", _ + 1)
+            for i in range(num_teams // 2):
+                team1 = self.teams[i]
+                team2 = self.teams[num_teams - i - 1]
+                if team1 is not None and team2 is not None:
+                    print(team1, "vs", team2)
+            self.rotate_teams()
+
+    def rotate_teams(self):
+        num_teams = len(self.teams)
+        rotating_team = self.teams.pop(num_teams - 1)
+        self.teams.insert(1, rotating_team)
