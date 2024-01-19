30cc17c501d2c4b837f0491357b4bc081dfd7b22
Owen Marchywka <67765181+dcfalcons2025@users.noreply.github.com>
2024-01-18 23:59:06 -0500

    added comments

--- league.py
+++ league.py
@@ -1,11 +1,16 @@
 class League:
+    
+    # Initializes a new instance of the class.
     def __init__(self):
         self.teams = []
 
+    # Adds a team to the list of teams.
     def add_team(self, team):
         if team not in self.teams:
             self.teams.append(team)
 
+
+    #Schedule a round-robin tournament for the teams.
     def schedule_round_robin(self):
         num_teams = len(self.teams)
         if num_teams % 2 != 0:
@@ -19,7 +24,8 @@ class League:
                 if team1 is not None and team2 is not None:
                     print(team1, "vs", team2)
             self.rotate_teams()
-
+     
+     #Rotates the teams in the tournament by moving the last team in the list to the second position.
     def rotate_teams(self):
         num_teams = len(self.teams)
         rotating_team = self.teams.pop(num_teams - 1)
